0384a39b2cc49867b88c47bc713859a06b68e764
Tom Szilagyi <tomszilagyi@gmail.com>
2017-01-30 19:20:57 +0100

    gui: Use splitter windows instead of sash windows

--- ds_gui.erl
+++ ds_gui.erl
@@ -16,23 +16,8 @@
         {
           config,
           frame,
-          panel,
-          panel_left,
-          panel_left_div,
-          panel_right,
-          panel_right_div,
-          sash_top_left,
-          sash_bottom_left,
-          sash_top_right,
-          sash_bottom_right
+          panel
         }).
-
--define(PANEL_LEFT,        10).
--define(PANEL_RIGHT,       20).
--define(SASH_TOP_LEFT,     30).
--define(SASH_BOTTOM_LEFT,  40).
--define(SASH_TOP_RIGHT,    50).
--define(SASH_BOTTOM_RIGHT, 60).
 
 start_link() ->
     Server = wx:new(),
@@ -54,12 +39,12 @@ do_init([Server] = Config) ->
     SizerOpts = [{flag, ?wxEXPAND}, {proportion, 1}],
 
     %% Left panel
-    LeftPanel = wxPanel:new(Splitter, [{winid, ?PANEL_LEFT}]),
+    LeftPanel = wxPanel:new(Splitter, []),
+    LeftSplitter = wxSplitterWindow:new(LeftPanel, []),
     LeftSizer = wxBoxSizer:new(?wxVERTICAL),
     wxPanel:setSizer(LeftPanel, LeftSizer),
 
-    TopLeftSash = wxSashWindow:new(LeftPanel, [{id, ?SASH_TOP_LEFT}, {style, ?wxSW_3D}]),
-    TopLeftPanel = wxPanel:new(TopLeftSash, []),
+    TopLeftPanel = wxPanel:new(LeftSplitter, []),
     TopLeftSizer = wxBoxSizer:new(?wxVERTICAL),
     wxPanel:setSizer(TopLeftPanel, TopLeftSizer),
 
@@ -81,8 +66,7 @@ do_init([Server] = Config) ->
     wxListCtrl:setItem(LC1, 2, 1, "123,456"),
     wxListCtrl:connect(LC1, command_list_item_selected, []),
 
-    BottomLeftSash = wxSashWindow:new(LeftPanel, [{id, ?SASH_BOTTOM_LEFT}, {style, ?wxSW_3D}]),
-    BottomLeftPanel = wxPanel:new(BottomLeftSash, []),
+    BottomLeftPanel = wxPanel:new(LeftSplitter, []),
     BottomLeftSizer = wxBoxSizer:new(?wxVERTICAL),
     wxPanel:setSizer(BottomLeftPanel, BottomLeftSizer),
 
@@ -98,32 +82,27 @@ do_init([Server] = Config) ->
     wxListCtrl:setItem(LC2, 0, 1, "123,456"),
     wxListCtrl:connect(LC2, command_list_item_selected, []),
 
-    wxSashWindow:setSashVisible(TopLeftSash, ?wxSASH_BOTTOM, true),
-    wxPanel:connect(LeftPanel, sash_dragged),
-    wxPanel:connect(LeftPanel, size),
-
-    wxSizer:add(LeftSizer, TopLeftSash, SizerOpts),
-    wxSizer:add(LeftSizer, BottomLeftSash, SizerOpts),
+    wxSplitterWindow:splitHorizontally(LeftSplitter, TopLeftPanel, BottomLeftPanel),
+    wxSplitterWindow:setSashGravity(LeftSplitter, 0.5),
+    wxSplitterWindow:setMinimumPaneSize(LeftSplitter, 1),
+    wxSizer:add(LeftSizer, LeftSplitter, SizerOpts),
 
     %% Right panel
-    RightPanel = wxPanel:new(Splitter, [{winid, ?PANEL_RIGHT}]),
+    RightPanel = wxPanel:new(Splitter, []),
+    RightSplitter = wxSplitterWindow:new(RightPanel, []),
     RightSizer = wxBoxSizer:new(?wxVERTICAL),
     wxPanel:setSizer(RightPanel, RightSizer),
 
-    TopRightSash = wxSashWindow:new(RightPanel, [{id, ?SASH_TOP_RIGHT}, {style, ?wxSW_3D}]),
-    Win3 = wxPanel:new(TopRightSash, []),
-    wxStaticText:new(Win3, ?wxID_ANY, "Statistics visualization", []),
+    TopRightPanel = wxPanel:new(RightSplitter, []),
+    wxStaticText:new(TopRightPanel, ?wxID_ANY, "Statistics visualization", []),
 
-    BottomRightSash = wxSashWindow:new(RightPanel, [{id, ?SASH_BOTTOM_RIGHT}, {style, ?wxSW_3D}]),
-    Win4 = wxPanel:new(BottomRightSash, []),
-    wxStaticText:new(Win4, ?wxID_ANY, "Private data visualization", []),
+    BottomRightPanel = wxPanel:new(RightSplitter, []),
+    wxStaticText:new(BottomRightPanel, ?wxID_ANY, "Private data visualization", []),
 
-    wxSashWindow:setSashVisible(TopRightSash, ?wxSASH_BOTTOM, true),
-    wxPanel:connect(RightPanel, sash_dragged),
-    wxPanel:connect(RightPanel, size),
-
-    wxSizer:add(RightSizer, TopRightSash, SizerOpts),
-    wxSizer:add(RightSizer, BottomRightSash, SizerOpts),
+    wxSplitterWindow:splitHorizontally(RightSplitter, TopRightPanel, BottomRightPanel),
+    wxSplitterWindow:setSashGravity(RightSplitter, 0.5),
+    wxSplitterWindow:setMinimumPaneSize(RightSplitter, 1),
+    wxSizer:add(RightSizer, RightSplitter, SizerOpts),
 
     %% Main vertical splitter
     wxSplitterWindow:splitVertically(Splitter, LeftPanel, RightPanel),
@@ -132,11 +111,7 @@ do_init([Server] = Config) ->
     wxSizer:add(Sizer, Splitter, SizerOpts),
 
     wxFrame:show(Frame),
-    {Frame, #state{config=Config, frame=Frame, panel=Panel,
-                   panel_left = LeftPanel, panel_left_div = 0.5,
-                   panel_right = RightPanel, panel_right_div = 0.5,
-                   sash_top_left = TopLeftSash, sash_bottom_left = BottomLeftSash,
-                   sash_top_right = TopRightSash, sash_bottom_right = BottomRightSash}}.
+    {Frame, #state{config=Config, frame=Frame, panel=Panel}}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Async Events are handled in handle_event as in handle_info
@@ -145,33 +120,6 @@ handle_event(#wx{obj = _ListCtrl,
              State = #state{}) ->
     io:format("Item ~p selected.~n", [Item]),
     {noreply,State};
-
-handle_event(#wx{id=?SASH_TOP_LEFT, event = #wxSash{dragRect = DragRect}},
-             State = #state{panel_left = Panel,
-                            sash_top_left = TopSash,
-                            sash_bottom_left = BottomSash}) ->
-    PanelDiv = handle_sash_event(Panel, TopSash, BottomSash, DragRect),
-    {noreply, State#state{panel_left_div=PanelDiv}};
-handle_event(#wx{id=?SASH_TOP_RIGHT, event = #wxSash{dragRect = DragRect}},
-             State = #state{panel_right = Panel,
-                            sash_top_right = TopSash,
-                            sash_bottom_right = BottomSash}) ->
-    PanelDiv = handle_sash_event(Panel, TopSash, BottomSash, DragRect),
-    {noreply, State#state{panel_right_div=PanelDiv}};
-handle_event(#wx{id=?PANEL_LEFT, event = #wxSize{size = Size}},
-             State = #state{panel_left = Panel,
-                            panel_left_div = PanelDiv,
-                            sash_top_left = TopSash,
-                            sash_bottom_left = BottomSash}) ->
-    handle_size_event(Panel, TopSash, BottomSash, PanelDiv, Size),
-    {noreply, State};
-handle_event(#wx{id=?PANEL_RIGHT, event = #wxSize{size = Size}},
-             State = #state{panel_right = Panel,
-                            panel_right_div = PanelDiv,
-                            sash_top_right = TopSash,
-                            sash_bottom_right = BottomSash}) ->
-    handle_size_event(Panel, TopSash, BottomSash, PanelDiv, Size),
-    {noreply, State};
 
 handle_event(#wx{} = Event, State = #state{}) ->
     io:format(user, "Event ~p~n", [Event]),
@@ -202,22 +150,3 @@ terminate(_Reason, _State) ->
 %% Local functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-handle_sash_event(Panel, TopSash, BottomSash, {_X, Y, _W, H}) ->
-    {PanelW, PanelH} = wxPanel:getSize(Panel),
-    BottomH = PanelH - H,
-    {PanelX, _} = wxSashWindow:getPosition(BottomSash),
-    wxSashWindow:setMinSize(BottomSash, {PanelW, BottomH}),
-    wxSashWindow:setMinSize(TopSash, {PanelW, H}),
-    wxSashWindow:setSize(BottomSash, {PanelX, Y, PanelW, BottomH}),
-    wxSashWindow:setSize(TopSash, {PanelW, H}),
-    wxPanel:refresh(Panel),
-    H/PanelH.
-
-handle_size_event(Panel, TopSash, BottomSash, PanelDiv, {W, H}) ->
-    TopH = round(H * PanelDiv),
-    BottomH = H - TopH,
-    wxSashWindow:setMinSize(TopSash, {W, TopH}),
-    wxSashWindow:setMinSize(BottomSash, {W, BottomH}),
-    wxSashWindow:setSize(TopSash, {W, TopH}),
-    wxSashWindow:setSize(BottomSash, {0, TopH, W, BottomH}),
-    wxPanel:refresh(Panel).
