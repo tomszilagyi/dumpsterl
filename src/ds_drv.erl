a24702262badf3a6d9c5c4b7935c5e00d36ae3cc
Tom Szilagyi <tomszilagyi@gmail.com>
2017-03-04 01:25:59 +0100

    Create the fold fun only once, not on every chunk
    This is mostly a change that makes the code clearer,
    the performance improvement it brings is negligible.

--- src/ds_drv.erl
+++ src/ds_drv.erl
@@ -73,36 +73,37 @@ fold_disk_log(Fun, Acc0, Filename, Spec0, Limit) ->
                                ]),
     Progress = ds_progress:init(),
     Spec = normalize_spec(Spec0),
-    fold_disk_log(Fun, Acc0, Spec, Limit, Progress, start).
+    FoldFun = fold_kernel(Fun, Spec),
+    fold_disk_log_loop(FoldFun, Acc0, Limit, Progress, start).
 
-fold_disk_log(_Fun, Acc, _Spec, 0, Progress, _Cont) ->
+fold_disk_log_loop(_FoldFun, Acc, 0, Progress, _Cont) ->
     ok = disk_log:close(dlog),
     ds_progress:final(Progress, Acc);
-fold_disk_log(Fun, Acc0, Spec, Limit, Progress0, Cont0) ->
+fold_disk_log_loop(FoldFun, Acc0, Limit, Progress0, Cont0) ->
     case disk_log:chunk(dlog, Cont0) of
         eof -> %% trigger end clause by setting Limit to 0
-            fold_disk_log(Fun, Acc0, Spec, 0, Progress0, undefined);
+            fold_disk_log_loop(FoldFun, Acc0, 0, Progress0, undefined);
         {error, Reason} ->
             io:format("disk_log: chunk failed: ~p~n", [Reason]),
             %% trigger end clause by setting Limit to 0
-            fold_disk_log(Fun, Acc0, Spec, 0, Progress0, undefined);
+            fold_disk_log_loop(FoldFun, Acc0, 0, Progress0, undefined);
         {Cont, RecL} ->
-            do_fold_disk_log(Fun, Acc0, Spec, Limit, Progress0, Cont, RecL);
+            do_fold_disk_log(FoldFun, Acc0, Limit, Progress0, Cont, RecL);
         {Cont, RecL, BadBytes} ->
             io:format("disk_log: skipped ~B bad bytes~n", [BadBytes]),
-            do_fold_disk_log(Fun, Acc0, Spec, Limit, Progress0, Cont, RecL)
+            do_fold_disk_log(FoldFun, Acc0, Limit, Progress0, Cont, RecL)
     end.
 
-do_fold_disk_log(Fun, Acc0, Spec, Limit, Progress0, Cont, RecL) ->
+do_fold_disk_log(FoldFun, Acc0, Limit, Progress0, Cont, RecL) ->
     case {ds_progress:get_count(Progress0), RecL} of
         %% ignore log_header entry at the start
         {0, [{log_header, dcd_log, _, _, _, _}|RestRecL]} ->
-            do_fold_disk_log(Fun, Acc0, Spec, Limit, Progress0, Cont, RestRecL);
+            do_fold_disk_log(FoldFun, Acc0, Limit, Progress0, Cont, RestRecL);
         _ ->
             RecN = length(RecL),
             Progress = ds_progress:update(Progress0, Acc0, RecN),
-            Acc = fold_kernel(Fun, Acc0, RecL, Spec),
-            fold_disk_log(Fun, Acc, Spec, counter_dec(Limit, RecN), Progress, Cont)
+            Acc = lists:foldl(FoldFun, Acc0, RecL),
+            fold_disk_log_loop(FoldFun, Acc, counter_dec(Limit, RecN), Progress, Cont)
     end.
 
 %% tuple of accessor funs to iterate tables:
@@ -135,34 +136,33 @@ fold_tab(Fun, Acc0, Type, Tab, Spec0, Limit) ->
     {FirstF, _ReadF, _NextF} = Accessors = accessors(Type),
     Progress = ds_progress:init(),
     Spec = normalize_spec(Spec0),
-    fold_tab(Fun, Acc0, Accessors, Tab, Spec, Limit, Progress, FirstF(Tab)).
+    FoldFun = fold_kernel(Fun, Spec),
+    fold_tab(FoldFun, Acc0, Accessors, Tab, Limit, Progress, FirstF(Tab)).
 
-fold_tab(_Fun, Acc, _Accessors, _Tab, _Spec, 0, Progress, _Key) ->
+fold_tab(_FoldFun, Acc, _Accessors, _Tab, 0, Progress, _Key) ->
     ds_progress:final(Progress, Acc);
-fold_tab(_Fun, Acc, _Accessors, _Tab, _Spec, _Limit, Progress, '$end_of_table') ->
+fold_tab(_FoldFun, Acc, _Accessors, _Tab, _Limit, Progress, '$end_of_table') ->
     ds_progress:final(Progress, Acc);
-fold_tab(Fun, Acc0, {_FirstF, ReadF, NextF} = Accessors,
-         Tab, Spec, Limit, Progress0, Key) ->
+fold_tab(FoldFun, Acc0, {_FirstF, ReadF, NextF} = Accessors,
+         Tab, Limit, Progress0, Key) ->
     RecL = ReadF(Tab, Key),
     RecN = length(RecL),
     Progress = ds_progress:update(Progress0, Acc0, RecN),
-    Acc = fold_kernel(Fun, Acc0, RecL, Spec),
-    fold_tab(Fun, Acc, Accessors, Tab, Spec,
+    Acc = lists:foldl(FoldFun, Acc0, RecL),
+    fold_tab(FoldFun, Acc, Accessors, Tab,
              counter_dec(Limit, RecN), Progress, NextF(Tab, Key)).
 
 %% The kernel for folding a list of records into the accumulated spec
-fold_kernel(Fun, Acc0, RecL, {FieldSpec, AttrSpecs}) ->
-    FoldF =
-        fun(Rec, FoldAcc) ->
-            %% The element accessor might crash or throw an error
-            %% which will exclude this instance from the spec.
-            try Field = elem(FieldSpec, Rec),
-                Attrs = attrs(AttrSpecs, Rec),
-                Fun({Field, Attrs}, FoldAcc)
-            catch _:_ -> FoldAcc
-            end
-        end,
-    lists:foldl(FoldF, Acc0, RecL).
+fold_kernel(Fun, {FieldSpec, AttrSpecs}) ->
+    fun(Rec, FoldAcc) ->
+        %% The element accessor might crash or throw an error
+        %% which will exclude this instance from the spec.
+        try Field = elem(FieldSpec, Rec),
+             Attrs = attrs(AttrSpecs, Rec),
+             Fun({Field, Attrs}, FoldAcc)
+        catch _:_ -> FoldAcc
+        end
+    end.
 
 %% If an attribute cannot be obtained via elem/2, just ignore it.
 attrs(AttrSpecs, Rec) ->
