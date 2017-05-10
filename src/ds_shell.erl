d789ca539dbf9e80a1c07d8dedfa6cdcda1981fb
Tom Szilagyi <tomszilagyi@gmail.com>
2017-05-10 12:21:27 +0200

    Shell: validate table when setting it and when starting probe

--- src/ds_shell.erl
+++ src/ds_shell.erl
@@ -22,8 +22,8 @@
 -define(DEFAULT_OPTIONS, [dump, {progress, 0.5}]).
 
 -record(state,
-        { type       = mnesia
-        , table      = widget_mnesia
+        { type
+        , table
         , field      = 0
         , attrs      = [] %[{ts, 3}, {key, 2}]
         , options    = ?DEFAULT_OPTIONS
@@ -39,7 +39,28 @@ start() ->
     ds_opts:setopts(?DEFAULT_OPTIONS),
     ds_records:init(),
     set_statusline_pid(spawn_link(?MODULE, statusline_srv, [])),
-    repl(#state{}).
+    DefaultType = ets,
+    DefaultTable = default_table(DefaultType),
+    repl(#state{type = DefaultType, table = DefaultTable}).
+
+default_table(ets) ->
+    case ets:all() of
+        [] -> undefined;
+        AllTables ->
+            case lists:filter(fun is_atom/1, AllTables) of
+                [FirstAtom|_] -> FirstAtom;
+                [] -> hd(AllTables)
+            end
+    end;
+default_table(mnesia) ->
+    case catch mnesia:system_info(tables) of
+        {'EXIT', {aborted, _}} -> undefined;
+        [schema] -> undefined; %% can't read the schema via mnesia!
+        Tables -> hd(lists:filter(fun(schema) -> false;
+                                     (_) -> true
+                                  end, Tables))
+    end;
+default_table(_Type) -> undefined.
 
 repl(State0) ->
     Line = io:get_line(?PROMPT),
@@ -87,14 +108,11 @@ eval_cmd("show" ++ ParamsStr, State) ->
              end,
     [show_param(P, State) || P <- Params],
     State;
-eval_cmd("run\n",
-         #state{type = Type, table = Table, field = Field, attrs = Attrs,
-                options = Options} = State) ->
-    FieldSpec = {Field, Attrs},
-    RecAttrs = ds_records:get_attrs(),
-    {ProbePid, ProbeMon} = ds_probe:start(Type, Table, FieldSpec, Options,
-                                          RecAttrs, get_statusline_pid()),
-    State#state{probe_pid = ProbePid, probe_monitor = ProbeMon};
+eval_cmd("run\n", State) ->
+    case check_params(State) of
+        true  -> run_probe(State);
+        false -> State
+    end;
 eval_cmd("stop\n", State) -> stop_probe(State);
 eval_cmd("help\n", State) -> help(State);
 eval_cmd("help " ++ ParamStr, State) -> help(ParamStr, State);
@@ -108,7 +126,11 @@ set_param("type"=Param, ValueStr, State) ->
     end;
 set_param("table"=Param, ValueStr, State) ->
     case parse_term(ValueStr) of
-        {ok, Value} -> show_param(Param, State#state{table = Value});
+        {ok, Value} ->
+            case check_param(Param, Value, State) of
+                true  -> show_param(Param, State#state{table = Value});
+                false -> State
+            end;
         {error, ReasonStr} -> io:format("~s~n", [ReasonStr]),
                               State
     end;
@@ -154,6 +176,57 @@ set_opt(OptStr, Value, #state{options=Options0} = State) ->
             invalid_param(OptStr),
             State
     end.
+
+%% Check all params that cannot be immediately validated on their input.
+%% Return true if everything is ok (probe can be started);
+%% return false and print message if a problem is found.
+check_params(#state{table=Table} = State) ->
+    check_param("table", Table, State).
+
+%% Check if supplied parameter is valid; return true | false accordingly.
+%% If parameter is invalid, print appropriate error message.
+check_param("table"=_Param, schema, #state{type=mnesia}) ->
+    io:format("Can't process schema table via mnesia; read it as a dets file!\n"),
+    false;
+check_param("table"=_Param, Value, #state{type=mnesia}) ->
+    case catch mnesia:table_info(Value, attributes) of
+        {'EXIT', {aborted, {no_exists, Value, _}}} ->
+            io:format("~p does not look like a mnesia table!\n", [Value]),
+            false;
+        _Attrs -> true
+    end;
+check_param("table"=_Param, Value, #state{type=ets}) ->
+    case lists:member(Value, ets:all()) of
+        false ->
+            io:format("~p does not look like an ets table!\n", [Value]),
+            false;
+        true -> true
+    end;
+check_param("table"=_Param, Value, #state{type=dets}) ->
+    case filelib:is_regular(Value) andalso dets:is_dets_file(Value) of
+        false ->
+            case dets:info(Value) of
+                undefined ->
+                    io:format("~p does not look like a dets table!\n", [Value]),
+                    false;
+                _InfoList -> true
+            end;
+        true -> true
+    end;
+check_param("table"=_Param, Value, #state{type=disk_log}) ->
+    try true = filelib:is_regular(Value),
+        {ok, dlog_test} = disk_log:open([ {name, dlog_test}
+                                        , {file, Value}
+                                        , {mode, read_only}
+                                        , {repair, false}
+                                        ]),
+        ok = disk_log:close(dlog_test),
+        true
+    catch error:{badmatch, _} ->
+        io:format("~p does not look like a disk_log file!\n", [Value]),
+        false
+    end.
+
 
 show_param("type"=Param, #state{type=Type}=State) ->
     io:format("~10s: ~p~n", [Param, Type]), State;
@@ -236,6 +309,15 @@ parse_term(Str) ->
     end.
 
 error_string({_Loc,_Mod, Error}) -> Error.
+
+
+run_probe(#state{type = Type, table = Table, field = Field, attrs = Attrs,
+                 options = Options} = State) ->
+    FieldSpec = {Field, Attrs},
+    RecAttrs = ds_records:get_attrs(),
+    {ProbePid, ProbeMon} = ds_probe:start(Type, Table, FieldSpec, Options,
+                                          RecAttrs, get_statusline_pid()),
+    State#state{probe_pid = ProbePid, probe_monitor = ProbeMon}.
 
 stop_probe(#state{probe_pid = undefined} = State) ->
     io:format("probe is not running\n"),
